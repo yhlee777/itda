23ecac9a578ab45443e0399b6552bc691c31c9de
yhllee777 <andrews2022@naver.com>
2025-09-26 22:03:03 +0900

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
-import dynamic from 'next/dynamic';
-
-const HomePage = dynamic(() => import('./HomePage'), {
-  ssr: false,
-  loading: () => (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <div className="text-white">Loading...</div>
-    </div>
-  )
-});
+import HomePage from './HomePage';
 
 export default function Page() {
   return <HomePage />;
